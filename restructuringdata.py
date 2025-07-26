7842b157c89989503ac4ad6a949153c4caacf294
avyue <averyyue@stanford.edu>
2025-07-26 16:36:31 -0700

    Update restructuringdata.py

--- restructuringdata.py
+++ restructuringdata.py
@@ -59,8 +59,7 @@ def _():
 
 @app.cell
 def _(mo, pd):
-    notebook_path = mo.notebook_location()
-    data_dir = f'{notebook_path}/public'
+    data_dir = './public'
     files = ['ARTIST.csv', 'NAAL.csv', 'merk2020.csv', 'rodrigues2024.csv']
     dataframes = []
 
